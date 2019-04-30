a42d37e4ca336088620e6f89baf8cee4b811d0d1
Marsel <akhmadullin.marsel@gmail.com>
2019-04-30 15:54:36 +0300

    Update parse.sh

--- jenkins/parse.sh
+++ jenkins/parse.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 wget -q 'http://m.ahmadullin:Sun$357D@10.113.136.32/download_trbs/snapshots/devel/san/5.5-unified-asan/latest/build.xml'
 ks=$(cat *.xml | grep san-tizen-5.5-unified-asan_)
-ks="${ks:6:-5}_mobile-wayland-armv7l-tm1.ks"
-echo $ks
-rm -rf build.xml
+ks=$(echo $ks | cut -c5-41)
+ks=$ks"_mobile-wayland-armv7l-tm1.ks"
+echo ${ks}
+rm -rf build*
